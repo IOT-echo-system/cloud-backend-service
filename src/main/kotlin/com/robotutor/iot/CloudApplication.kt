d1a595ebd69fd75b94b3b3032c613179b414d7b4
Shivam Rajput <raj.shiviraj@gmail.com>
2024-05-21 07:00:52 +0530

    Shivi | Enable scheduler.

--- src/main/kotlin/com/robotutor/iot/CloudApplication.kt
+++ src/main/kotlin/com/robotutor/iot/CloudApplication.kt
@@ -3,11 +3,13 @@ package com.robotutor.iot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = ["com.robotutor.iot"])
 @EnableWebFlux
+@EnableScheduling
 class CloudApplication {
     companion object {
         @JvmStatic
